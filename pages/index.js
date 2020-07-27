d9582575b45a6db8895d939b46c1b67b54388bad
Waraht Punyarote <waraht_pun@truecorp.co.th>
2020-07-27 09:37:11 +0700

    Routing is working properly.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-const Index = () => (
+import Link from 'next/link'
+
+const index = () => (
   <div>
     <p>Building Next.js with Express.js.</p>
+    <Link href="about">About</Link>
   </div>
 )
-export default Index
+export default index
